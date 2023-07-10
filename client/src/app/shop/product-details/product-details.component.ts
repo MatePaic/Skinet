dafca81b5b77c8cff43555eb3dfcd04d47926a7f
matepaic <mate.paic1997@gmail.com>
2023-07-10 09:02:08 +0200

    End of section 11: Breadcrumbs, bootstrap theme, loading indicators, adding home page

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,8 +14,11 @@ export class ProductDetailsComponent implements OnInit{
 
   constructor(
     private shopService: ShopService, 
-    private activatedRoute: ActivatedRoute
-  ) {}
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
+    this.bcService.set('@productDetails', ' ');
+  }
 
   ngOnInit(): void {
     this._loadProduct();
@@ -23,7 +27,10 @@ export class ProductDetailsComponent implements OnInit{
   private _loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name);
+      },
       error: error => console.log(error)
     });
   }
